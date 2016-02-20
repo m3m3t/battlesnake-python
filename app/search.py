80e9fee9bd9c4b916e85fae075d35582ffbbb9ea
Amanda Dash <memet666@gmail.com>
2016-02-20 14:54:08 -0800

    buggy

--- app/search.py
+++ app/search.py
@@ -102,7 +102,7 @@ def ping(grid, current, goals):
     subgraph = [(0,h,0,w),(0,h,w,c),(h,r,0,w),(h,r,w,c)]
     processes = [ _Process(target=a_star_search, args=(result, subgraph[i], current, goals[i])) for i in range(0,4) ]
     """
-    goal = goals[0]
+    goal = [1,1] 
     processes = [ _Process(target=a_star_search, args=(result, grid, current, goal)) ]
     
     for p in processes:
