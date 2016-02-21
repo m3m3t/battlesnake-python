94b1bfb4f514df9f44bf7aaf228bf6381d4f1c2a
Amanda Dash <memet666@gmail.com>
2016-02-20 16:44:09 -0800

    buggy

--- app/main.py
+++ app/main.py
@@ -44,6 +44,11 @@ def move():
     grid = SquareGrid(data["height"], data["width"])
     snakes = [ snake["coords"] for snake in data["snakes"]  ]
     grid.snakes = [ tuple(x) for x in snakes ]
+
+    for i, coord in enumerate(snakes):
+        if i == 0:
+            [x,y] = coord
+            grid.snakes.extend([(x+1, y), (x, y-1), (x-1, y), (x, y+1)])
     
     current = [ snake["coords"] for snake in data["snakes"] if snake["id"] == ID ][0]
     
